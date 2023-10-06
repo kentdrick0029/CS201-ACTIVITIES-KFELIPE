e9ef1dc20dca1d338ce6516a0b4198dc5c21e6d3
kentdrick0029 <146787064+kentdrick0029@users.noreply.github.com>
2023-10-06 14:29:48 +0800

    Add files via upload

---
+++ CS201-ACTIVITIES/act1.js
@@ -0,0 +1,11 @@
+let firstFavNumb = 29;
+let secondFavNumb = 17;
+
+console.log(firstFavNumb + secondFavNumb);
+console.log(firstFavNumb - secondFavNumb);
+console.log(firstFavNumb * secondFavNumb);
+console.log(firstFavNumb / secondFavNumb);
+console.log(firstFavNumb % secondFavNumb);
+
+let power = Math.pow(firstFavNumb);
+console.log(power)
---
+++ CS201-ACTIVITIES/act2.js
@@ -0,0 +1,27 @@
+let firstFavNum = 29;
+let secondFavNum = 29;
+
+if(firstFavNum > secondFavNum){
+    console.log("firstFavNum is greater than secondFavNum")
+}
+else if(firstFavNum < secondFavNum){
+    console.log("firstFavNum is less than secondFavNum")
+}
+else if(firstFavNum >= secondFavNum){
+    console.log("firstFavNum is greater than or equal to secondFavNum")
+}
+else if(firstFavNum <= secondFavNum){
+    console.log("firstFavNum is less than or equal to secondFavNum")
+}
+else if(firstFavNum === secondFavNum){
+    console.log("firstFavNum is equal to secondFavNum")
+}
+else if(firstFavNum == secondFavNum){
+    console.log("firstFavNum is not equal to secondFavNum")
+}
+else if(firstFavNum !== secondFavNum){
+    console.log("firstFavNum is not equal to secondFavNum")
+}
+else if(firstFavNum != secondFavNum){
+    console.log("firstFavNum is not equal to secondFavNum")
+}
